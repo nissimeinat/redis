46c250a73aae8bd247718733f4638fcf4fda6205
Yossi Gottlieb <yossigo@gmail.com>
2013-11-10 14:04:19 +0200

    Fix merge error: rdb_syncfilename not used.

--- src/replication.c
+++ src/replication.c
@@ -726,7 +726,7 @@ void updateSlavesWaitingBgsave(int bgsaveerr) {
                 redisLog(REDIS_WARNING,"SYNC failed. BGSAVE child returned an error");
                 continue;
             }
-            if ((slave->repldbfd = open(server.rdb_filename,O_RDONLY)) == -1 ||
+            if ((slave->repldbfd = open(server.rdb_syncfilename ? server.rdb_syncfilename : server.rdb_filename,O_RDONLY)) == -1 ||
                 redis_fstat(slave->repldbfd,&buf) == -1) {
                 freeClient(slave);
                 redisLog(REDIS_WARNING,"SYNC failed. Can't open/stat DB after BGSAVE: %s", strerror(errno));
