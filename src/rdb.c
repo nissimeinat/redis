0ec7d2de8e08294cae95d076738b239f8a94a867
Yossi Gottlieb <yossigo@gmail.com>
2013-11-24 12:45:32 +0200

    Update bgsave status only on explicit saves.

--- src/rdb.c
+++ src/rdb.c
@@ -1412,15 +1412,19 @@ void backgroundSaveDoneHandler(int exitcode, int bysignal) {
         redisLog(REDIS_WARNING,
             "Background saving terminated by signal %d", bysignal);
         rdbRemoveTempFile(server.rdb_child_pid);
-        if (update_status) server.lastbgsave_status = REDIS_ERR;
-
-        /* SIGUSR1 is whitelisted, so we have a way to kill a child without
-         * tirggering an error conditon. */
-        if (bysignal != SIGUSR1)
+        if (update_status) {
             server.lastbgsave_status = REDIS_ERR;
+
+            /* SIGUSR1 is whitelisted, so we have a way to kill a child without
+             * tirggering an error conditon. */
+            if (bysignal != SIGUSR1)
+                server.lastbgsave_status = REDIS_ERR;
+        }
     }
     server.rdb_child_pid = -1;
-    server.rdb_save_time_last = time(NULL)-server.rdb_save_time_start;
+    if (update_status) {
+        server.rdb_save_time_last = time(NULL)-server.rdb_save_time_start;
+    }
     server.rdb_save_time_start = -1;
     /* Possibly there are slaves waiting for a BGSAVE in order to be served
      * (the first stage of SYNC is a bulk transfer of dump.rdb) */
